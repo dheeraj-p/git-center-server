3bb48eaa084c84dc13d59794655d549a8bb89083
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-04-01 23:56:36 +0530

    Fix build_image script

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-docker build -t git-center-server 1236dheerajp/git-center-server:latest . \
-  --build-arg DATABASE_URI \
+docker build --build-arg DATABASE_URI \
   --build-arg SERVER_HOST \
   --build-arg SERVER_PORT \
   --build-arg SERVER_PROTOCOL \
   --build-arg REPOSITORIES_PATH \
-  --build-arg AUTHORIZED_KEY_FILE
+  --build-arg AUTHORIZED_KEY_FILE \
+  -t 1236dheerajp/git-center-server:latest ~/app
