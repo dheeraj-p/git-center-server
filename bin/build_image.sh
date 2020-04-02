aef0877496c2222658ebcb4771ce1de5e657ca75
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-04-02 22:19:10 +0530

    Configure deployment for shared volumes

--- bin/build_image.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker build --build-arg DATABASE_URI \
-  --build-arg SERVER_HOST \
-  --build-arg SERVER_PORT \
-  --build-arg SERVER_PROTOCOL \
-  --build-arg REPOSITORIES_PATH \
-  --build-arg AUTHORIZED_KEY_FILE \
-  -t 1236dheerajp/git-center-server:latest ~/app
