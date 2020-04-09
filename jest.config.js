d37cf6f901633e26f7ade81bd528ef539648c72f
Chandan Kumar <cksharma122@gmail.com>
2020-04-09 23:39:30 +0530

    Increasing jest coverage to 100%

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 10,
-      branches: 10,
-      functions: 10,
-      lines: 10,
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
     },
   },
   testEnvironment: 'node',
